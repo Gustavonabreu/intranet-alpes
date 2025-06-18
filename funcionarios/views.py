7ff67bc56f0cff44fd30693a41c926e3969f2b68
Gustavonabreu <gustavooonf@gmail.com>
2025-06-18 10:38:30 -0300

    Feat: cria e exibe a pagina inicial com aniversariantes do mes

--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,3 +1,18 @@
+# funcionarios/views.py
 from django.shortcuts import render
+from .models import Funcionario
+import datetime
 
-# Create your views here.
+def homepage(request):
+    # Pega a data de hoje
+    data_hoje = datetime.date.today()
+
+    # Filtra os funcionários que fazem aniversário no mês atual
+    aniversariantes_do_mes = Funcionario.objects.filter(data_nascimento__month=data_hoje.month)
+
+    # Cria um "contexto" para enviar os dados para o HTML
+    context = {
+        'aniversariantes': aniversariantes_do_mes,
+    }
+
+    return render(request, 'funcionarios/homepage.html', context)
